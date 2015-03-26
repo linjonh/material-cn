d801ae7f07fc8d4f280da0a7ebbd2bb980998603
马君保 <106@chinadance.com>
2015-03-26 23:09:47 +0800

    第一次上传

--- README.md
+++ README.md
@@ -1,8 +1,22 @@
 # Google Material Design
 
-#本网站制作步骤
-1 下载Google官方网站
-2 替换资源为本地资源，因为视频无法直接播放，所以把视频放到github服务器上。
-3 下载台湾人民翻译版本(git地址)
-4 翻译成中文版(git地址)
+## 本网站制作步骤
 
+1. 下载Google官方网站
+2. 替换资源为本地资源，因为视频无法直接播放，所以把视频放到github服务器上。
+3. 下载台湾人民翻译版本(git地址)
+4. 翻译成中文版(git地址)
+
+## 目录
+
+1.Material design
+  1.introduction
+2.What is material?
+3.Animation
+4.Style
+5.Layout
+6.Components
+7.Patterns
+8.Usability
+9.Resources
+10.What's new
--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var replace = require('gulp-replace');
 
 gulp.task('default',function(){
-  gulp.src(['./source-web/design/spec/**/**.html'])
+  gulp.src(['./source-en/design/spec/**/**.html'])
     .pipe(replace('/design/spec/','../'))
     .pipe(gulp.dest('./buid'));
 
@@ -10,6 +10,6 @@ gulp.task('default',function(){
     .pipe(replace('/design/spec/',''))
     .pipe(gulp.dest('./buid'));
 
-  gulp.src('./source-web/design/spec/static/**')
+  gulp.src('./source-en/design/spec/static/**')
     .pipe(gulp.dest('./buid/static'));
 })
